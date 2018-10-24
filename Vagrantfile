944f3c6e7a268324140f9898b2cd7683ba04679e
Alvaro Miranda Aguilera <kikitux@gmail.com>
2018-10-24 16:47:47 +0200

    update

--- Vagrantfile
+++ Vagrantfile
@@ -1,13 +1,19 @@
 Vagrant.configure("2") do |config|
   config.vm.box = "alvaro/xenial64"
-  config.vm.provision :shell, :path => "scripts/install_consul.sh", run: "always"
-  config.vm.provision :shell, :path => "scripts/install_vault.sh", run: "always"
-  config.vm.provision :shell, :inline => "cd /vagrant ; ./app.sh", run: "always"
+
+  # set VM specs
+  config.vm.provider "virtualbox" do |v|
+    v.memory = 1024
+    v.cpus = 2
+  end
+
+  config.vm.provision :shell, :path => "scripts/provision.sh"
+  config.vm.provision :shell, :path => "scripts/vault.sh", run: "always"
+  config.vm.provision :shell, :inline => "cd /vagrant ; ./wrapper.sh", run: "always"
 
   config.vm.define "leader01" do |l1|
       l1.vm.hostname = "leader01"
       l1.vm.network "private_network", ip: "192.168.2.10"
-      l1.vm.network "forwarded_port", guest: 8500, host: 8500
       l1.vm.network "forwarded_port", guest: 8200, host: 8200
   end
 
---
+++ scripts/provision.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+
+CONSUL=1.3.0
+CONSULTEMPLATE=0.19.5
+ENVCONSUL=0.7.3
+VAULT=0.11.4
+
+PKG="wget unzip"
+which ${PKG} &>/dev/null || {
+  export DEBIAN_FRONTEND=noninteractive
+  apt-get update
+  apt-get install -y ${PKG}
+}
+
+# check consul binary
+which consul || {
+  pushd /usr/local/bin
+  [ -f consul_${CONSUL}_linux_amd64.zip ] || {
+    sudo wget https://releases.hashicorp.com/consul/${CONSUL}/consul_${CONSUL}_linux_amd64.zip
+  }
+  sudo unzip consul_${CONSUL}_linux_amd64.zip
+  sudo chmod +x consul
+  popd
+}
+
+# check consul-template binary
+which consul-template || {
+  pushd /usr/local/bin
+  [ -f consul-template_${CONSULTEMPLATE}_linux_amd64.zip ] || {
+    sudo wget https://releases.hashicorp.com/consul-template/${CONSULTEMPLATE}/consul-template_${CONSULTEMPLATE}_linux_amd64.zip
+  }
+  sudo unzip consul-template_${CONSULTEMPLATE}_linux_amd64.zip
+  sudo chmod +x consul-template
+  popd
+}
+
+# check envconsul binary
+which envconsul || {
+  pushd /usr/local/bin
+  [ -f envconsul_${ENVCONSUL}_linux_amd64.zip ] || {
+    sudo wget https://releases.hashicorp.com/envconsul/${ENVCONSUL}/envconsul_${ENVCONSUL}_linux_amd64.zip
+  }
+  sudo unzip envconsul_${ENVCONSUL}_linux_amd64.zip
+  sudo chmod +x envconsul
+  popd
+}
+
+
+which vault &>/dev/null || {
+  pushd /usr/local/bin
+  [ -f vault_${VAULT}_linux_amd64.zip ] || {
+    sudo wget https://releases.hashicorp.com/vault/${VAULT}/vault_${VAULT}_linux_amd64.zip
+  }
+  sudo unzip vault_${VAULT}_linux_amd64.zip
+  sudo chmod +x vault
+  popd
+}
